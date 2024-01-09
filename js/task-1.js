c7d3c6b369b64a9ff98a22c28f5370ae4da24a8b
Sad-creature <yana.kravchuk.2000@gmail.com>
2024-01-09 06:31:29 +0200

    Add three tasks

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+}
+
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
